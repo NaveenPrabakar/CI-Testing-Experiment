d4ae9c91a5f095bf1e8b74ad319953477f180db4
Naveen Prabakar <nvnprabakar@gmail.com>
2025-11-28 16:15:22 -0600

    Ekstazi metadata

--- src/main/java/com/teamproject/calculator/Calculator.java
+++ src/main/java/com/teamproject/calculator/Calculator.java
@@ -20,4 +20,8 @@ public class Calculator {
         }
         return a / b;
     }
+
+    public int mod(int a, int b){
+        return a % b;
+    }
 }
--- src/test/java/com/teamproject/calculator/CalculatorTest.java
+++ src/test/java/com/teamproject/calculator/CalculatorTest.java
@@ -1,38 +1,37 @@
 package com.teamproject.calculator;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
     @Test
-    void testAdd() {
+    public void testAdd() {
         Calculator c = new Calculator();
         assertEquals(10, c.add(6, 4));
     }
 
     @Test
-    void testSub() {
+    public void testSub() {
         Calculator c = new Calculator();
         assertEquals(2, c.sub(6, 4));
     }
 
     @Test
-    void testMul() {
+    public void testMul() {
         Calculator c = new Calculator();
         assertEquals(24, c.mul(6, 4));
     }
 
     @Test
-    void testDiv() {
+    public void testDiv() {
         Calculator c = new Calculator();
         assertEquals(2, c.div(8, 4));
     }
 
-    @Test
-    void testDivByZero() {
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivByZero() {
         Calculator c = new Calculator();
-        assertThrows(IllegalArgumentException.class,
-                () -> c.div(5, 0));
+        c.div(5, 0);
     }
 }
