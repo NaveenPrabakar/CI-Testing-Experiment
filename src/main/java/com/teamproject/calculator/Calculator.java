1c37b0d0ee21e17c865dcaf68ae1e392e8f5a214
Naveen Prabakar <nvnprabakar@gmail.com>
2025-11-28 19:40:30 -0600

    fixed pom.xml

--- src/main/java/com/teamproject/calculator/Calculator.java
+++ src/main/java/com/teamproject/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.teamproject.calculator;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b + 1;
     }
 
     public int sub(int a, int b) {
